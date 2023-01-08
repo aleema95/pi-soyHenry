ffcb6b423decff8636b7f1f8bc3bd05f39a22b12
aleema95 <alejandro.manouellian@gmail.com>
2023-01-08 20:14:29 -0300

    added DB_PORT env variable

--- api/src/db.js
+++ api/src/db.js
@@ -3,7 +3,7 @@ const { Sequelize, Op } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
 const {
-  DB_USER, DB_PASSWORD, DB_HOST, DB_NAME
+  DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT
 } = process.env;
 
 //API URLkey = https://api.rawg.io/api/games?key=ad24d83c81c34279bcee55590c7f7124
@@ -13,7 +13,7 @@ let sequelize =
       database: DB_NAME,
       dialect: 'postgres',
       host: DB_HOST,
-      port: 5432,
+      port: DB_PORT || 5432,
       username: DB_USER,
       password: DB_PASSWORD,
       pool: {
